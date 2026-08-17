5c5c74a614ccebf814ac1051cff041ee247e70df
Parth Gorasiya <parthgorasiya77@gmail.com>
2026-08-17 18:37:32 -0500

    Build authentication service with appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import config from "../config/config.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+        .setEndpoint(config.appWriteUrl)
+        .setProject(config.appWriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount ({email, password, name}){
+        try{
+            
+           const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+           if(userAccount) {
+            //call another method
+            return this.login({email, password});
+           }
+           else{
+                return userAccount;
+           }
+        }
+        catch(err){
+            throw err;
+        }
+    }
+
+    async login({email, password}) {
+        try{
+            return await this.account.createEmailPasswordSession(email, password);
+        }
+        catch(err){
+            throw err;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+
+        }catch(err){
+            console.log("Appwrite Service :: getCurrentUser :: error ", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try{
+            await this.account.deleteSessions();
+
+        }catch(err){
+            throw err;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
